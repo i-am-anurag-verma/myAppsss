199db6ca48ca37aefb4fab3fbdaf206f713a541a
Anurag <anuragv83@gmail.com>
2022-11-27 12:39:40 +0530

    27th'Nov 12:39

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './component/Home';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
         {/* <Login text={{name:"Virat"}}   Data="Kohli"/> */}
         <Home />
     </div>
---
+++ src/component/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = (props) => {
+  return (
+    <div>
+        {props.Button}
+    </div>
+  )
+}
+
+export default Button
